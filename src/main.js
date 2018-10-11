bebd5b78407496c50a3697ff0a516f47cec18c50
dongchun <ldc84@naver.com>
2018-10-11 15:40:54 +0900

    업로드 테스트

--- src/main.js
+++ src/main.js
@@ -11,12 +11,12 @@ Vue.config.productionTip = false
   fill belows from the firebase project setup
 */
 var config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: ''
+  apiKey: 'AIzaSyB20KvGNExHfCWuiDrsAHXIBRX9WqYaEd8',
+  authDomain: 'vue-example1.firebaseapp.com',
+  databaseURL: 'https://vue-example1.firebaseio.com',
+  projectId: 'vue-example1',
+  storageBucket: 'vue-example1.appspot.com',
+  messagingSenderId: '686205830736'
 }
 firebase.initializeApp(config)
 
